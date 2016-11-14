e9ee3f834d404307858f765cdb8ed3c33db68ac1
Remi AGIER <remi.agier@creatis.insa-lyon.fr>
2016-11-14 17:11:03 +0100

    iniial commit

--- config.js
+++ config.js
@@ -1,72 +1,48 @@
+"use strict";
 /**
  * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-	// %REMOVE_START%
-	config.plugins =
-		'about,' +
-		'a11yhelp,' +
-		'basicstyles,' +
-		'bidi,' +
-		'blockquote,' +
-		'clipboard,' +
-		'colorbutton,' +
-		'colordialog,' +
-		'contextmenu,' +
-		'dialogadvtab,' +
-		'div,' +
-		'elementspath,' +
-		'enterkey,' +
-		'entities,' +
-		'filebrowser,' +
-		'find,' +
-		'flash,' +
-		'floatingspace,' +
-		'font,' +
-		'format,' +
-		'forms,' +
-		'horizontalrule,' +
-		'htmlwriter,' +
-		'image,' +
-		'iframe,' +
-		'indentlist,' +
-		'indentblock,' +
-		'justify,' +
-		'language,' +
-		'link,' +
-		'list,' +
-		'liststyle,' +
-		'magicline,' +
-		'maximize,' +
-		'newpage,' +
-		'pagebreak,' +
-		'pastefromword,' +
-		'pastetext,' +
-		'preview,' +
-		'print,' +
-		'removeformat,' +
-		'resize,' +
-		'save,' +
-		'selectall,' +
-		'showblocks,' +
-		'showborders,' +
-		'smiley,' +
-		'sourcearea,' +
-		'specialchar,' +
-		'stylescombo,' +
-		'tab,' +
-		'table,' +
-		'tabletools,' +
-		'templates,' +
-		'toolbar,' +
-		'undo,' +
-		'wysiwygarea';
-	// %REMOVE_END%
-};
 
-// %LEAVE_UNMINIFIED% %REMOVE_LINE%
+	// %REMOVE_START%
+	// The configuration options below are needed when running CKEditor from source files.
+	config.plugins = 'dialogui,dialog,basicstyles,clipboard,button,panelbutton,panel,floatpanel,colorbutton,colordialog,menu,contextmenu,resize,toolbar,enterkey,entities,popup,filebrowser,find,floatingspace,listblock,richcombo,font,htmlwriter,image,indent,indentblock,indentlist,justify,list,liststyle,magicline,pastetext,pastefromword,removeformat,showborders,specialchar,tab,table,tabletools,undo,tableresize,lineutils,widget,filetools,notification,notificationaggregator,uploadwidget,uploadimage,quicktable';
+	config.plugins += ',image2,raseyes';
+	// %REMOVE_END%
+
+
+	// Define changes to default configuration here. For example:
+	config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+  config.filebrowserImageBrowseUrl = config.filebrowserUploadUrl = '/api/cke/file';
+	config.uploadUrl = '/api/cke/file';
+	config.contentsCss = ['/styles/nomin/fonts.css', '/scripts/vendor/ckeditor_dev/contents.css'];
+	config.skin = 'office2013';
+
+	config.toolbar = [
+		{ name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+		{ name: 'editing', items: [ 'Find', 'Replace' ] },
+		'/',
+		{ name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+		{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
+		{ name: 'insert', items: [ 'Image', 'Raseyes', 'Table', 'SpecialChar' ] },
+		'/',
+		{ name: 'styles', items: [ 'Font', 'FontSize' ] },
+		{ name: 'colors', items: [ 'TextColor', 'BGColor' ] }
+	];
+
+	config.font_names =
+		'Arial/Arial, Helvetica, sans-serif;' +
+		'opendyslexic/opendyslexic;' +
+		'Cursive/CursiveCustom;' +
+		'Comic Sans MS/Comic Sans MS, cursive;' +
+		'Courier New/Courier New, Courier, monospace;' +
+		//'Georgia/Georgia, serif;' +
+		//'Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif;' +
+		//'Tahoma/Tahoma, Geneva, sans-serif;' +
+		'Times New Roman/Times New Roman, Times, serif;' +
+		//'Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;' +
+		'Verdana/Verdana, Geneva, sans-serif';
+};
---
+++ plugins/raseyes/plugin.js
@@ -0,0 +1,25 @@
+"use strict";
+CKEDITOR.plugins.add( 'raseyes', {
+    requires: 'widget',
+    icons:'raseyes',
+    init: function( editor ) {
+      editor.widgets.add( 'raseyes', {
+          button: 'Create seyes paragraph',
+          template:
+            '<p class="seyes-content">' +
+              '' +
+            '</p>',
+          editables: {
+              content: {
+                  selector: '.seyes-content',
+                  allowedContent: 'br strong em'
+              }
+          },
+          allowedContent:'p(!seyes-content)',
+          requiredContent: 'p(seyes-content)',
+          upcast: function( element ) {
+              return element.name == 'p' && element.hasClass( 'seyes-content' );
+          }
+      } );
+    }
+} );
